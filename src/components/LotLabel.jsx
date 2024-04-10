0096f2d80c7ebe93f59df828376ef4be87577cbd
George Luther <george.e.luther@gmail.com>
2024-04-10 00:23:04 -0400

    WORKING! lotLabel page renders uri query params!

--- src/components/LotLabel.jsx
+++ src/components/LotLabel.jsx
@@ -1,9 +1,14 @@
+//Test URI: lotlabel?lotId=dHVhjE7f28cJ3NNNB27Dsu&sku=PAINT0016&jobName=GARD%2010574&jobId=23717&release=1of1&kit=23717-R1-K1
+
 import { useSearchParams } from "react-router-dom";
 
 function LotLabel() {
   let [searchParams] = useSearchParams();
-  let { lotId, sku, jobName, jobId, release, kit } = searchParams;
-
+  const allParams = {};
+  for (const [key, value] of searchParams.entries()) {
+    allParams[key] = value;
+  }
+  const { lotId, sku, jobName, jobId, release, kit } = allParams;
   console.log("lotId:", lotId);
   return (
     <div className="label">
